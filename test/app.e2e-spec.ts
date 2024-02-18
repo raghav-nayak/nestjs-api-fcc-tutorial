3cf7b9da2c136d32edcc50b1d4f3fe57d3109af8
Raghav Nayak <ragsnayak@gmail.com>
2024-02-19 00:55:38 +0530

    added cleanup db function and added declared different functions to test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,22 +1,51 @@
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
 import { AppModule } from "../src/app.module";
+import { PrismaService } from "../src/prisma/prisma.service";
 
 describe("App e2e", () => {
     let app: INestApplication;
+    let prisma: PrismaService;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         const moduleRef = await Test.createTestingModule({
             imports: [AppModule],
         }).compile();
         app = moduleRef.createNestApplication();
         app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
         await app.init();
+
+        prisma = app.get(PrismaService);
+        await prisma.cleanDb(); // to clean the database
     });
 
     afterAll(() => {
         app.close();
     });
 
-    it.todo("it should pass");
+    describe("Auth", () => {
+        describe("Signup", () => {
+            it.todo("Should signup");
+        });
+
+        describe("Signin", () => {
+            it.todo("Should signin");
+        });
+    });
+
+    describe("User", () => {
+        describe("Who am I", () => {});
+
+        describe("Edit user", () => {});
+    });
+
+    describe("Bookmark", () => {
+        describe("Create bookmark", () => {});
+
+        describe("Get bookmark by id", () => {});
+
+        describe("Edit bookmark", () => {});
+
+        describe("Delete bookmark", () => {});
+    });
 });
